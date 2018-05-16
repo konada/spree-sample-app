d862fbbfc79fbd4373658c1786e4c1d7d9616e9b
konada <konrad.cisak@gmail.com>
2018-05-16 16:55:02 +0200

    add spree analytics extension

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'spree_analytics_trackers', github: 'spree-contrib/spree_analytics_trackers'
 
 group :development, :test do
   gem 'better_errors'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110102631) do
+ActiveRecord::Schema.define(version: 20180516143133) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
